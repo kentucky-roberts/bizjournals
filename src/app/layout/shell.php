2f93a535da6ed7b135704084d87451cd638d2b24
croberts <croberts@bizjournals.com>
2016-04-11 13:05:01 -0400

    fixed css and js links on shell.php

--- src/app/layout/shell.php
+++ src/app/layout/shell.php
@@ -5,17 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Home</title>
-
-    <!-- Bootstrap -->
-    <link href="../../lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <title>Shell</title>
+    <link href="../../../lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
     <?php include('bar-header.php'); ?>
@@ -25,7 +16,7 @@
       <div class="row">
         <div class="col-lg-12">
 
-          <h1><?php $title = "Home"; print($title); ?></h1>
+          <h1><?php $title = "Shell"; print($title); ?></h1>
 
 
         </div><!--/.col-lg-12 -->
@@ -38,10 +29,9 @@
 
 
 
-    <script src="../../lib/jquery/dist/jquery.min.js"></script>
-    <script src="../../lib/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="../app.module.js"></script>
-    <script src="../app.config.js"></script>
+    <script src="../../../lib/jquery/dist/jquery.min.js"></script>
+    <script src="../../../lib/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../app.js"></script>
     <script src="../app.core.js"></script>
   </body>
 </html>
